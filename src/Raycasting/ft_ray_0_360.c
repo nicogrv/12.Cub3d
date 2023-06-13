7bf20524f929a61b26355200e16eb901274a4e02
Nicolas GRIVEAU <nicolasgri45@icloud.com>
2023-06-13 11:41:39 +0200

    0.0001

--- src/Raycasting/ft_ray_0_360.c
+++ src/Raycasting/ft_ray_0_360.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ray_0_360.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nicolasgriveau <nicolasgriveau@student.    +#+  +:+       +#+        */
+/*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 14:23:20 by ngriveau          #+#    #+#             */
-/*   Updated: 2023/06/08 23:35:17 by nicolasgriv      ###   ########.fr       */
+/*   Updated: 2023/06/13 11:39:52 by ngriveau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ void	ft_ray_270_360(t_data *d)
 	d->hyr = ((d->posx - (int)d->posx)) / cos((90 - d->ax) / RAD);
 	if (d->hyv < d->hyr)
 	{
-		d->decaly = (1 - (d->posy - (int)d->posy)) + 0.000001;
+		d->decaly = (1 - (d->posy - (int)d->posy)) + 0.001;
 		d->decalx = ((1 - (d->posy - (int)d->posy)) * -1) * \
-						tan((d->ax) / RAD) + 0.000001 ;
+						tan((d->ax) / RAD) + 0.001 ;
 		d->length += d->hyv;
 		d->face = 3;
 		d->pcofwall = 100 - ((int)((d->posx + d->decalx - \
@@ -31,8 +31,8 @@ void	ft_ray_270_360(t_data *d)
 	}
 	else
 	{
-		d->decaly = (d->posx - (int)d->posx) * tan((90 - d->ax) / RAD) - 0.000001;
-		d->decalx = ((d->posx - (int)d->posx) * -1) - 0.000001;
+		d->decaly = (d->posx - (int)d->posx) * tan((90 - d->ax) / RAD) - 0.001;
+		d->decalx = ((d->posx - (int)d->posx) * -1) - 0.001;
 		d->length += d->hyr;
 		d->face = 4;
 		d->pcofwall = 100 - ((int)((d->posy + d->decaly - \
@@ -50,9 +50,9 @@ void	ft_ray_180_270(t_data *d)
 	d->hyr = (1 - (d->posx - (int)d->posx)) / cos(d->ax / RAD);
 	if (d->hyv < d->hyr)
 	{
-		d->decaly = (1 - (d->posy - (int)d->posy)) + 0.000001;
+		d->decaly = (1 - (d->posy - (int)d->posy)) + 0.001;
 		d->decalx = (1 - (d->posy - (int)d->posy)) * \
-				tan((90 - d->ax) / RAD) + 0.000001;
+				tan((90 - d->ax) / RAD) + 0.001;
 		d->length += d->hyv;
 		d->face = 3;
 		d->pcofwall = 100 - ((int)((d->posx + d->decalx - \
@@ -60,8 +60,8 @@ void	ft_ray_180_270(t_data *d)
 	}
 	else
 	{
-		d->decaly = (1 - (d->posx - (int)d->posx)) * tan((d->ax) / RAD) + 0.000001;
-		d->decalx = (1 - (d->posx - (int)d->posx)) + 0.000001;
+		d->decaly = (1 - (d->posx - (int)d->posx)) * tan((d->ax) / RAD) + 0.001;
+		d->decalx = (1 - (d->posx - (int)d->posx)) + 0.001;
 		d->length += d->hyr ;
 		d->face = 2;
 		d->pcofwall = (int)((d->posy + d->decaly - \
@@ -79,8 +79,8 @@ void	ft_ray_90_180(t_data *d)
 	d->hyr = (1 - (d->posx - (int)d->posx)) / cos((90 - d->ax) / RAD);
 	if (d->hyv < d->hyr)
 	{
-		d->decaly = ((d->posy - (int)d->posy) * -1) - 0.000001;
-		d->decalx = (d->posy - (int)d->posy) * tan((d->ax) / RAD) - 0.000001;
+		d->decaly = ((d->posy - (int)d->posy) * -1) - 0.001;
+		d->decalx = (d->posy - (int)d->posy) * tan((d->ax) / RAD) - 0.001;
 		d->length += d->hyv;
 		d->face = 1;
 		d->pcofwall = (int)((d->posx + d->decalx - \
@@ -89,8 +89,8 @@ void	ft_ray_90_180(t_data *d)
 	else
 	{
 		d->decaly = ((1 - (d->posx - (int)d->posx)) * -1) * \
-				tan((90 - d->ax) / RAD) + 0.000001;
-		d->decalx = (1 - (d->posx - (int)d->posx)) + 0.000001;
+				tan((90 - d->ax) / RAD) + 0.001;
+		d->decalx = (1 - (d->posx - (int)d->posx)) + 0.001;
 		d->length += d->hyr;
 		d->face = 2;
 		d->pcofwall = (int)((d->posy + d->decaly - \
@@ -103,9 +103,9 @@ void	ft_ray_0_90_pt2(t_data *d)
 {
 	if (d->hyv < d->hyr)
 	{
-		d->decaly = ((d->posy - (int)d->posy) * -1) - 0.000001;
+		d->decaly = ((d->posy - (int)d->posy) * -1) - 0.001;
 		d->decalx = ((d->posy - (int)d->posy) * -1) * \
-						tan((90 - d->ax) / RAD) - 0.00001;
+						tan((90 - d->ax) / RAD) - 0.001;
 		d->length += d->hyv;
 		d->face = 1;
 		d->pcofwall = (int)((d->posx + d->decalx - \
@@ -114,8 +114,8 @@ void	ft_ray_0_90_pt2(t_data *d)
 	else
 	{
 		d->decaly = ((d->posx - (int)d->posx) * -1) * \
-						tan((d->ax) / RAD) - 0.000001;
-		d->decalx = ((d->posx - (int)d->posx) * -1) - 0.000001;
+						tan((d->ax) / RAD) - 0.001;
+		d->decalx = ((d->posx - (int)d->posx) * -1) - 0.001;
 		d->length += d->hyr;
 		d->face = 4;
 		d->pcofwall = 100 - ((int)((d->posy + d->decaly - \
