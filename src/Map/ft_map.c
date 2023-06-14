642d3ac30a472c240c80b1a50ce11b44afbce09f
Nicolas GRIVEAU <nicolasgri45@icloud.com>
2023-06-14 12:32:40 +0200

    free

--- src/Map/ft_map.c
+++ src/Map/ft_map.c
@@ -6,7 +6,7 @@
 /*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 14:23:20 by ngriveau          #+#    #+#             */
-/*   Updated: 2023/06/12 21:35:45 by ngriveau         ###   ########.fr       */
+/*   Updated: 2023/06/14 12:32:24 by ngriveau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,7 @@ int	ft_map(t_data *data)
 	while (y < data->mapy)
 	{
 		if (ft_map_pt2(data, &y, x, fd))
-			return (close(fd), 1);
+			return (close(fd), ft_free_map(data, y+1), 1);
 	}
 	close(fd);
 	return (0);
